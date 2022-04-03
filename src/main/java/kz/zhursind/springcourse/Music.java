91d5ea5442b617eba5bda719d9968e69fb1fd91e
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-04-03 18:52:34 +0600

    Inversion of Control

---
+++ src/main/java/kz/zhursind/springcourse/Music.java
@@ -0,0 +1,5 @@
+package kz.zhursind.springcourse;
+
+public interface Music {
+  String getSong();
+}
---
+++ src/main/java/kz/zhursind/springcourse/RockMusic.java
@@ -0,0 +1,8 @@
+package kz.zhursind.springcourse;
+
+public class RockMusic implements Music {
+
+  @Override public String getSong() {
+    return "Wind cries Mary";
+  }
+}
