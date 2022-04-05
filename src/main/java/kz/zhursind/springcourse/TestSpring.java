129435f12e5fed4d352779061c6e8aacfe2b0cee
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-04-05 17:21:02 +0600

    Lifecycle

--- src/main/java/kz/zhursind/springcourse/TestSpring.java
+++ src/main/java/kz/zhursind/springcourse/TestSpring.java
@@ -8,24 +8,28 @@ public class TestSpring {
         "applicationContext.xml"
     );
 
+    ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+
+    System.out.println(classicalMusic.getSong());
+
     //Music music = context.getBean("musicBean", Music.class);
 
     //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-    MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-    MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-    boolean comparison = firstMusicPlayer == secondMusicPlayer;
-
-    System.out.println(comparison);
-
-    System.out.println(firstMusicPlayer);
-    System.out.println(secondMusicPlayer);
-
-    firstMusicPlayer.setVolume(40);
-
-    System.out.println(firstMusicPlayer.getVolume());
-    System.out.println(secondMusicPlayer.getVolume());
+//    MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//    MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//    boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//
+//    System.out.println(comparison);
+//
+//    System.out.println(firstMusicPlayer);
+//    System.out.println(secondMusicPlayer);
+//
+//    firstMusicPlayer.setVolume(40);
+//
+//    System.out.println(firstMusicPlayer.getVolume());
+//    System.out.println(secondMusicPlayer.getVolume());
 
 
     //musicPlayer.playMusic();
