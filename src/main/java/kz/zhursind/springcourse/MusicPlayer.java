4d7986689ecf815677691bf82de0d740453d7f68
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-04-05 17:06:40 +0600

    Bean scope Prototype

--- src/main/java/kz/zhursind/springcourse/MusicPlayer.java
+++ src/main/java/kz/zhursind/springcourse/MusicPlayer.java
@@ -1,11 +1,8 @@
 package kz.zhursind.springcourse;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MusicPlayer {
 
-  private List<Music> musicList = new ArrayList<>();
+  private Music music;
 
   private String name;
   private int volume;
@@ -27,19 +24,17 @@ public class MusicPlayer {
   }
 
   // IoC
-//  public MusicPlayer(Music music) {
-//    this.music = music;
-//  }
+  public MusicPlayer(Music music) {
+    this.music = music;
+  }
 
   public MusicPlayer() {}
 
-  public void setMusicList(final List<Music> musicList) {
-    this.musicList = musicList;
+  public void setMusic(final Music music) {
+    this.music = music;
   }
 
-  public void playMusicList() {
-    for (Music music : musicList) {
-      System.out.println("Playing: " + music.getSong());
-    }
+  public void playMusic() {
+    System.out.println("Playing: " + music.getSong());
   }
 }
--- src/main/java/kz/zhursind/springcourse/TestSpring.java
+++ src/main/java/kz/zhursind/springcourse/TestSpring.java
@@ -12,8 +12,23 @@ public class TestSpring {
 
     //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-    MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-    musicPlayer.playMusicList();
+    MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+    MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+    boolean comparison = firstMusicPlayer == secondMusicPlayer;
+
+    System.out.println(comparison);
+
+    System.out.println(firstMusicPlayer);
+    System.out.println(secondMusicPlayer);
+
+    firstMusicPlayer.setVolume(40);
+
+    System.out.println(firstMusicPlayer.getVolume());
+    System.out.println(secondMusicPlayer.getVolume());
+
+
+    //musicPlayer.playMusic();
     context.close();
   }
 }
